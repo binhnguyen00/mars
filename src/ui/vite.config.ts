a69b20194e2ceda081eb1e5370c9747cb33f2d5a
binhnguyen00 <jackjack2000.kahp@gmail.com>
2024-09-05 00:22:18 +0700

    config vite

---
+++ src/ui/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  root: path.resolve(__dirname),                            // Set the root to current dir
+  publicDir: path.resolve(__dirname, 'public'),             // this is default, dont have to add
+  cacheDir: path.resolve(__dirname, 'node_modules/.vite'),  // this is default, dont have to add
+  plugins: [
+    react()
+  ],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: path.resolve(__dirname, './dist'),              // this is default, dont have to add
+    emptyOutDir: true,
+  }
+});
