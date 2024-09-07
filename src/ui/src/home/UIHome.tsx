91293fdc493a9473102e374d4bcd0268346383a8
binhnguyen00 <jackjack2000.kahp@gmail.com>
2024-09-07 13:32:13 +0700

    reactor code

--- src/ui/src/home/UIHome.tsx
+++ src/ui/src/home/UIHome.tsx
@@ -1,78 +1,17 @@
-import React, { ReactEventHandler } from "react";
-import { restful } from "@/server/RESTful";
+import React from "react";
+
+const UIImageConverter = React.lazy(() => import("@/image/UIImageConverter"));
+// more UI lazy load
 
 export function UIHome() {
-  const [loading, setLoading] = React.useState(false);
-  const [error, setError] = React.useState<string | null>(null);
-  const [resultImageUrl, setResultImageUrl] = React.useState<string | null>(null);
-  const [downloadFileName, setDownloadFileName] = React.useState<string | null>(null);
-  const [orgImageExt, setOrgImageExt] = React.useState<string>("");
-  const [targetImageExt, setTargetImageExt] = React.useState<string>("");
+  const [ container, setContainer ] = React.useState<React.ReactElement>(<UIImageConverter/>);
 
-  const getFileExtension = (fileName: string) => {
-    const extension = fileName.split('.').pop();
-    return extension || "";
-  };
+  const renderContainer = React.useCallback(() => {
+    return container;
+  }, [container]);
 
-  const getImageInputFile = () => {
-    const fileInput = (document.getElementById('image') as HTMLInputElement).files?.[0];
-    return fileInput || null;
-  };
-
-  const setFormat = (format: string) => {
-    console.log(format);
-    setTargetImageExt(format);
-  }
-
-  const upload = async (event: React.MouseEvent) => {
-    event.preventDefault();
-    setLoading(true);
-    setError(null); // Reset any previous errors
-    setResultImageUrl(null); // Reset the result image
-
-    const imageFile = getImageInputFile();
-    if (!imageFile) {
-      setError("Please select an image file.");
-      setLoading(false);
-      return;
-    }
-
-    const body = new FormData();
-    body.append("image", imageFile);
-    body.append("format", targetImageExt);
-    try {
-      const response = await restful.POST('/upload-image', body);
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-
-      const blob = await response.blob();
-      const url = URL.createObjectURL(blob);
-      setResultImageUrl(url);
-
-      const processedFileName = imageFile.name.replace(/\.[^/.]+$/, "") + "_processed";
-      setDownloadFileName(processedFileName);
-    } catch (err: any) {
-      setError(`Upload failed: ${err.message}`);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const downloadFile = () => {
-    if (resultImageUrl && downloadFileName) {
-      const a = document.createElement('a');
-      a.href = resultImageUrl;
-      a.download = downloadFileName;
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-    }
-  };
-
-  return (
+  return(
     <React.Fragment>
-      
       <header>
         <hgroup style={{ margin: 0 }}>
           <h1> Image Converter </h1>
@@ -81,66 +20,12 @@ export function UIHome() {
         <hr />
       </header>
 
-      
-      <main style={{ flexGrow: 1 }}>
-        <div>
-          <label> Select an image file </label>
-          <input type="file" id="image" accept="image/*" required onInput={() => {
-            const imageFile = getImageInputFile();
-            if (imageFile) {
-              const extension = getFileExtension(imageFile.name);
-              setOrgImageExt(extension);
-            }
-          }}/>
-        </div>
-        <div className="grid">
-          <div>
-            <label htmlFor="input-formart"> Input file format </label>
-            <input name="input-formart" value={orgImageExt.toUpperCase()} disabled/>
-          </div>
-          <div>
-            <label> New file format </label>
-            <select required defaultValue={"Select new format"} onChange={(event) => {
-              setTargetImageExt(event.target.value);
-            }}>
-              <option disabled> Select new format </option>
-              <option value={"PNG"}>PNG</option>
-              <option value={"JPG"}>JPG</option>
-              <option value={"JPEG"}>JPEG</option>
-            </select>
-          </div>
-        </div>
-        <button onClick={upload} disabled={loading} className="outline">
-          {loading ? "Uploading..." : "Upload"}
-        </button>
-
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-
-        <div className="flex-v">
-          <h4 style={{ textAlign: 'center' }}> Your Converted Image </h4>
-          {resultImageUrl ? (
-            <img 
-              id="resultImage" alt="Processed" src={resultImageUrl} 
-              width={"100%"} height={"auto"}
-            />
-          ) : (
-            <img 
-              id="resultImage" alt="Processed" src={"../../assets/imgs/placeholder.svg"} 
-              width={"auto"} height={"auto"}
-            />
-          )}
-          {resultImageUrl && (
-            <button onClick={downloadFile}>Download</button>
-          )}
-        </div>
-      </main>
-
+      {renderContainer()}
 
       <footer>
         <hr />
         <p>In Development</p>
       </footer>
-
     </React.Fragment>
-  );
+  )
 }
