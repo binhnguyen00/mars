f4b889ef58ea2340394f3592067d28ec88792590
binhnguyen00 <jackjack2000.kahp@gmail.com>
2024-09-07 10:18:25 +0700

    refactor code

--- src/module/server/server.py
+++ src/module/server/server.py
@@ -1,44 +1,19 @@
-import os
-import src.module.image.processor as ImgProcessor
+import src.module.server.service as ServerService
 
-from flask import (Flask, request, jsonify, send_from_directory, send_file)
+from flask import (Flask, request)
 from flask_cors import CORS
-from src.app.context import (ROOT_DIR, UPLOAD_FOLDER, RESULT_FOLDER)
 
 server = Flask(__name__)
 CORS(server)
 
-@server.route('/', methods=['GET'])
-def welcome():
-  file_path = os.path.join(ROOT_DIR, 'ui/public')
-  return send_from_directory(directory=file_path, path='index.html')
-
 @server.route('/upload-image', methods=['POST'])
 def upload_image():
-  if 'image' not in request.files:
-    return jsonify({
-      'error': 'No image part in the request'
-    }), 400
-
-  file = request.files['image']
-  if file.filename == '' or not file.filename:
-    return jsonify({
-      'error': 'No selected file'
-    }), 400
-  else:
-    format = request.form.get('format') or 'PNG'
-    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
-    file.save(file_path)
-    result_image = ImgProcessor.process_image(file_path, format)
-    response = send_file(result_image, as_attachment=True)
-    return response
-
+  return ServerService.upload_image(request)
 
 @server.route('/get-image/<image_name>', methods=['GET'])
 def get_image(image_name):
-  directory = os.path.join(RESULT_FOLDER, image_name)
-  print(directory)
-  return send_file(directory, as_attachment=True)
+  return ServerService.get_image(image_name)
+
 
 def run(debug=True, host='0.0.0.0', port=5000):
   server.run(debug=debug, host=host, port=port)
