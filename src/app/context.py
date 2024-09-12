ccc8b63a2a6b8e699cd81120dd56bece56622617
binhnguyen00 <jackjack2000.kahp@gmail.com>
2024-09-12 16:56:07 +0700

    rename file storage

--- src/app/context.py
+++ src/app/context.py
@@ -3,6 +3,10 @@ import os
 # Get the 'src' directory
 SRC_DIR         = os.path.join(os.path.dirname(os.path.dirname(__file__)))
 
-IMG_UPLOAD_DIR  = os.path.join(SRC_DIR, 'module/image/resources/uploads')
-IMG_RESULT_DIR  = os.path.join(SRC_DIR, 'module/image/resources/results')
+IMG_UPLOAD_DIR  = os.path.join(SRC_DIR, 'module/image/storage/uploads')
+IMG_RESULT_DIR  = os.path.join(SRC_DIR, 'module/image/storage/results')
+
+ARCHIVE_UPLOAD_DIR  = os.path.join(SRC_DIR, 'module/archive/storage/uploads')
+ARCHIVE_RESULT_DIR  = os.path.join(SRC_DIR, 'module/archive/storage/results')
+
 APP_CONFIG_DIR  = os.path.join(SRC_DIR, 'app/config/application.yaml')
