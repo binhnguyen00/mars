77fa0e605629aaafe53c8147e03a75b221cbace5
binhnguyen00 <jackjack2000.kahp@gmail.com>
2024-09-12 16:56:33 +0700

    add componnent scan

--- src/module/server/service.py
+++ src/module/server/service.py
@@ -4,8 +4,15 @@ from src.module.database.db import db;
 from src.module.server.initial import server;
 
 def run_server(debug=False, host='0.0.0.0', port=5000):
+  # Component Scan
+  import src.module.archive.controller;
+  import src.module.image.controlller;
+  from src.module.image.table.image import Image
+  from src.module.archive.table.archive import Archive 
+
   ImgProcessor.create_result_folder()
   ImgProcessor.create_storing_folder()
   with server.app_context():
     db.create_all()
+    print("Tables created successfully")
   server.run(debug=debug, host=host, port=port)
