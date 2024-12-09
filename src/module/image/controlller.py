526a91941ed67806fd42d438045f7d5f7460ec69
binhnguyen00 <jackjack2000.kahp@gmail.com>
2024-12-09 11:05:03 +0700

    clean code

--- src/module/image/controlller.py
+++ src/module/image/controlller.py
@@ -6,7 +6,3 @@ from src.module.server.initial import server;
 @server.route('/upload-image', methods=['POST'])
 def upload_image():
   return ImageService.upload_image(request)
-
-@server.route('/get-image/<image_name>', methods=['GET'])
-def get_image(image_name):
-  return ImageService.get_image(image_name)
